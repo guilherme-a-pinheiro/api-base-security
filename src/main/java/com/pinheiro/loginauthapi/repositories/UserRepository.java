1600de16c8fda7fd4c182ea89a3496854b8abff7
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-05-19 19:09:40 -0300

    SecurityFilter criado e metodo findByEmail criado no UserRepository

--- src/main/java/com/pinheiro/loginauthapi/repositories/UserRepository.java
+++ src/main/java/com/pinheiro/loginauthapi/repositories/UserRepository.java
@@ -4,5 +4,8 @@ import com.pinheiro.loginauthapi.domain.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String login);
 }
