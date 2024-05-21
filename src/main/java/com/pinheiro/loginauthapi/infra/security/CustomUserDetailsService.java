d4a11fc044659754ad38fdbc7072f1a5e17ce003
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-05-20 22:20:08 -0300

    CustomUserDetailsService criado, para validar o usuario dentro do banco

---
+++ src/main/java/com/pinheiro/loginauthapi/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.pinheiro.loginauthapi.infra.security;
+
+import com.pinheiro.loginauthapi.domain.User.User;
+import com.pinheiro.loginauthapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayDeque;
+
+public class CustomUserDetailsService  implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayDeque<>());
+    }
+}
